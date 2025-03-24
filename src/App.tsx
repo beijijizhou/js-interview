83a5859195df283ba67ee2ab099d6ad1ac9ccc60
“beijijizhou” <hongzhong.hu@stonybrook.edu>
2025-03-24 10:30:50 -0400

    first init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="p-6 max-w-4xl mx-auto">
+      <h1 className="text-3xl font-bold text-blue-500">JS Interview Prep</h1>
+      <p className="mt-4 text-gray-700">Practice JS quirks like hoisting and closures!</p>
+    </div>
+  );
+}
+
+export default App;
